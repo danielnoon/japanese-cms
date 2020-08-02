4cc307a73187246414e6ea34f545ac89dfbaceea
Daniel Noon <supercookies@outlook.com>
2020-08-01 21:27:24 -0500

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '26cce34a03945916e5edf6df9e71f771'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'df38c5cf-2a84-467a-8dc3-0fc2070d7cf4'
+};
